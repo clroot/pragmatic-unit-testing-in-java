c6afff6ef3b6f1799b80c2d773f5c67e0a89028c
clroot <abcdkh1209@gmail.com>
2021-08-31 16:15:32 +0900

    ch1.3 테스트 준비, 실행, 단언

--- src/test/java/ScoreCollectionTest.java
+++ src/test/java/ScoreCollectionTest.java
@@ -1,11 +1,21 @@
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class ScoreCollectionTest {
 
     @Test
     public void test() {
-        fail("Not yet implemented");
+        // 준비
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // 실행
+        int actualResult = collection.arithmeticMean();
+
+        // 단언
+        assertThat(actualResult, equalTo(6));
     }
 }
