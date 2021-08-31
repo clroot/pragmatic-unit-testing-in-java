7f033560e74d9600956794b8501d2de79897b55e
clroot <abcdkh1209@gmail.com>
2021-08-31 18:11:43 +0900

    ch2.5 @Before 메서드로 테스트 초기화
    - 여러개의 테스트에서 중복된 로직을 가지고 있다면 @Before 메서드로 이동시키기

--- src/test/java/ProfileTest.java
+++ src/test/java/ProfileTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -5,18 +6,26 @@ import static org.junit.Assert.assertTrue;
 
 public class ProfileTest {
 
+    private Profile profile;
+
+    private BooleanQuestion question;
+
+    private Criteria criteria;
+
+    @Before
+    public void create() {
+        profile = new Profile("Bull Hockey, Inc. ");
+        question = new BooleanQuestion(1, "Got bonuses?");
+        criteria = new Criteria();
+    }
+
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-        Profile profile = new Profile("Bull Hockey, Inc. ");
-        Question question = new BooleanQuestion(1, "Got bonuses?");
-
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
 
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-
         criteria.add(criterion);
 
         boolean matches = profile.matches(criteria);
@@ -26,16 +35,11 @@ public class ProfileTest {
 
     @Test
     public void matchAnswerTrueForAnyDontCareCriteria() {
-        Profile profile = new Profile("Bull Hockey, Inc. ");
-        Question question = new BooleanQuestion(1, "Got milk?");
-
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
 
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
-
         criteria.add(criterion);
 
         boolean matches = profile.matches(criteria);
