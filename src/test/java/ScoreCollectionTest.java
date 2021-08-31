c0385b2c472daf3ca23fba1c577edbf38347ec3f
clroot <abcdkh1209@gmail.com>
2021-08-31 16:07:59 +0900

    ch1.2 JUnit의 기본

---
+++ src/test/java/ScoreCollectionTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class ScoreCollectionTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
