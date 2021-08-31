2d4c49038d40f1e61260e7f05b83ad86b9a174bd
clroot <abcdkh1209@gmail.com>
2021-08-31 18:03:40 +0900

    ch2.4 두 번째 테스트 만들기
    - ch2.3에서 작성한 테스트와 유사하지만
    - criterion의 가중치가 DontCare인 경우 확인

--- src/test/java/ProfileTest.java
+++ src/test/java/ProfileTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ProfileTest {
 
@@ -21,5 +22,24 @@ public class ProfileTest {
         boolean matches = profile.matches(criteria);
 
         assertFalse(matches);
+    }
+
+    @Test
+    public void matchAnswerTrueForAnyDontCareCriteria() {
+        Profile profile = new Profile("Bull Hockey, Inc. ");
+        Question question = new BooleanQuestion(1, "Got milk?");
+
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+
+        assertTrue(matches);
     }
 }
